90e718f797dec87c4ccdc70fceff226f39dd1624
m-suzuki <ma_suzuki@braingate.onmicrosoft.com>
2025-11-26 15:19:13 +0900

    fix:Case15.java 受講生 初回ログイン 利用規約に不同意テストコード追加
    WebElementを利用してDBに初期登録された受講生ユーザーの利用規約の同意にチェックを入れず、エラーメッセージが表示されるかどうか確認するテストを作成した。
    期待値は以下の通り
    ログインID：StudentAA02
    パスワード：StudentAA02
    エラーメッセージ：セキュリティ規約への同意は必須です。
    
    fix:結合テスト仕様書の修正(ケース15を追記)

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能②
@@ -36,6 +39,16 @@ public class Case15 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		//画面遷移
+		goTo("http://localhost:8080/lms");
+
+		//タイトルの値とログインボタンの値が一致しているかどうか
+		String title = webDriver.getTitle();
+		assertEquals("ログイン | LMS", title);
+
+		//ログイン画面のエビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
@@ -43,13 +56,55 @@ public class Case15 {
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		//ログインの際に必要な値取得
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement login = webDriver.findElement(By.className("btn"));
+
+		//ログインの際に入力する値
+		loginId.clear();
+		loginId.sendKeys("StudentAA02");
+		password.clear();
+		password.sendKeys("StudentAA02");
+		login.click();
+
+		//利用規約に遷移したかどうかの確認
+		final String loginUser = webDriver.findElement(By.xpath("//*[@id=\"main\"]/h2")).getText();
+		assertEquals("利用規約", loginUser);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックをせず「次へ」ボタンを押下")
-	void test03() {
+	void test03() throws InterruptedException {
 		// TODO ここに追加
+		//指定位置までスクロール
+		scrollBy("1000");
+
+		Thread.sleep(5000);
+
+		//同意します」チェックボックスにチェックを入れず、「次へ」ボタン押下
+		final WebElement submit = webDriver
+				.findElement(By.xpath("//*[@id=\"main\"]/div[2]/form/fieldset/div[2]/button"));
+		submit.click();
+
+		//エラーメッセージが表示されたかどうか
+		final String errorMessage = webDriver
+				.findElement(
+						By.cssSelector("#main > div:nth-child(5) > form > fieldset > div:nth-child(1) > div.error"))
+				.getText();
+		assertEquals("セキュリティ規約への同意は必須です。", errorMessage);
+
+		//指定位置までスクロール
+		scrollBy("1000");
+
+		Thread.sleep(10000);
+
+		getEvidence(new Object() {
+		});
 	}
 
 }
