29a67d3fd1f350763a72c59711a0c91151fb93ac
m-suzuki <ma_suzuki@braingate.onmicrosoft.com>
2025-11-28 15:42:08 +0900

    fix:Case10.java 受講生 勤怠登録 正常系テストコード追加
    WebElementを利用して勤怠の入力を出勤ボタンと退勤ボタンでできるかどうか確認するテストを作成した。
    期待値は以下の通り
    勤怠管理画面で出勤ボタンと退勤ボタンをそれぞれ押し、反映されるかどうか
    
    fix:結合テスト仕様書の修正(ケース10を追記)

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -36,34 +40,122 @@ public class Case10 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		//画面遷移
+		goTo("http://localhost:8080/lms");
+
+		//タイトルの値とログインボタンの値が一致しているかどうか
+		String title = webDriver.getTitle();
+		assertEquals("ログイン | LMS", title);
+
+		//ログイン画面のエビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
-	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
+	@DisplayName("テスト02 管理者アカウントでログイン")
 	void test02() {
 		// TODO ここに追加
+		//ログインの際に必要な値取得
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement login = webDriver.findElement(By.className("btn"));
+
+		//ログインの際に入力する値
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+		password.clear();
+		password.sendKeys("StudentAA0");
+		login.click();
+
+		//ログインできているかどうかの確認
+		final String loginUser = webDriver
+				.findElement(
+						By.cssSelector("#nav-content > ul.nav.navbar-nav.navbar-right > li:nth-child(2) > a > small"))
+				.getText();
+		assertEquals("ようこそ受講生ＡＡ１さん", loginUser);
+
+		//ログイン後の表示待ち
+		visibilityTimeout(By.className("btn"), 10);
+		getEvidence(new Object() {
+		});
 	}
 
+	@SuppressWarnings("static-access")
 	@Test
 	@Order(3)
-	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
+	@DisplayName("テスト03 上部メニューの「勤怠」リンクから、勤怠管理画面に遷移する。")
 	void test03() {
 		// TODO ここに追加
+		//「勤怠」リンクを押下
+		final WebElement Attendance = webDriver
+				.findElement(
+						By.cssSelector("#nav-content > ul:nth-child(1) > li:nth-child(3) > a"));
+		Attendance.click();
+
+		//アラート受け入れ
+		Alert Alert = webDriver.switchTo().alert();
+		Alert.accept();
+
+		//勤怠管理画面に遷移できてるかどうか
+		final String AttendanceManagement = webDriver
+				.getCurrentUrl();
+		assertEquals("http://localhost:8080/lms/attendance/detail", AttendanceManagement);
+
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(4)
-	@DisplayName("テスト04 「出勤」ボタンを押下し出勤時間を登録")
+	@DisplayName("テスト04 「出勤」ボタンを押下し、出勤時間を登録する。")
 	void test04() {
 		// TODO ここに追加
+		//「出勤」ボタンを押下
+		final WebElement ClockInButton = webDriver
+				.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[1]/div[2]/form/input[1]"));
+		ClockInButton.click();
+
+		//アラート受け入れ
+		Alert Alert = webDriver.switchTo().alert();
+		Alert.accept();
+
+		//出勤時間が反映されているかどうか
+		final WebElement ClockInCheck = webDriver
+				.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[3]/div/table/tbody/tr[2]/td[3]"));
+		final boolean userDisplayed = ClockInCheck.isDisplayed();
+		assertTrue(userDisplayed);
+
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(5)
-	@DisplayName("テスト05 「退勤」ボタンを押下し退勤時間を登録")
-	void test05() {
+	@DisplayName("テスト05 「退勤」ボタンを押下し、退勤時間を登録する。")
+	void test05() throws InterruptedException {
 		// TODO ここに追加
+		//「退勤」ボタンを押下
+		final WebElement ClockOutButton = webDriver
+				.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[2]/div[2]/form/input[2]"));
+		ClockOutButton.click();
+
+		//アラート受け入れ
+		Alert Alert = webDriver.switchTo().alert();
+		Alert.accept();
+
+		//退勤時間が反映されているかどうか
+		final WebElement ClockInCheck = webDriver
+				.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[3]/div/table/tbody/tr[2]/td[4]"));
+		final boolean userDisplayed = ClockInCheck.isDisplayed();
+		assertTrue(userDisplayed);
+
+		getEvidence(new Object() {
+		});
+
 	}
 
 }
